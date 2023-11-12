5c3dd16db5ebe295e573fff458811dfdd579c798
Joce <jocelynn4355@gmail.com>
2023-11-11 22:37:04 -0600

    added new typeDefs and resolvers and mutations, started the server page

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const User = require('../models/User');
+const { signToken } = require('../utils/auth');
+const resolvers = {
+    Query:{
+        me: async (parent, args, context) => {
+            if ( context.user ) {
+                // if user exists get user info based on args
+                const existingUser = await User.findOne({ _id: context.user._id })
+                return existingUser
+            }
+            throw new Error("Not logged in")
+        }
+    },
+    Mutation:{
+        logIn: async (parent, { email, password }) => {
+            // find user by unique email
+            const existingUser = await User.findOne({ email })
+            if ( !existingUser ) {
+                throw new Error("Invalid email")
+            }
+            const correctPassword = await existingUser.isCorrectPassword(password)
+            if ( !correctPassword ) {
+                throw new Error("Incorrect password")
+            }
+            const token = signToken(existingUser);
+            // made sure user exists, password is correct, assaign token to user allowing login, then return new token with user information
+            return{ token, user: existingUser }
+        },
+        addUser: async (parent, args) => {
+            const newUser = await User.create(args)
+            const token = signToken(newUser);
+            return { token, user: newUser}
+        },
+        saveBook: async (parents, args, context) => {
+            const data = {
+                authors: args.authors,
+                description: args.description,
+                bookId: args.bookId,
+                image: args.image,
+                link: args.link,
+                title: args.title
+            }
+            if (context.user) {
+                const existingUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { saveBook: data }},
+                    { new: true }
+                )
+                return existingUser;
+            } throw new Error("Not logged in");
+        },
+        removeBook: async (parents, { bookId }, context) => {
+            if (context.user) {
+                const existingUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { saveBook: { bookId }}},
+                    { new: true }
+                )
+                 return existingUser;
+            } throw new Error("Not logged in");
+        }
+    }
+}
---
+++ server/schemas/typedef.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    saveBooks: [ Book ]
+}
+type Book {
+    bookId: ID
+    authors: [ String ]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+type Auth {
+    token: ID
+    user: User
+}
+type Query {
+    me: User
+}
+type Mutations {
+    logIn(email: String, password: String): Auth
+    addUser(email: String, password: String, username: String): Auth
+    saveBook(bookId: ID, authors: [ String ], description: String, image: String, link: String, title: String): User
+    removeBook(bookId: ID): User
+}
+`
+module.exports = typeDefs
